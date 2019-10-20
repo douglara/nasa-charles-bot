817169892e954797df4d029ecb1e63f88512f70e
Douglas Lara <douglasdelaras@gmail.com>
2019-10-19 21:28:50 -0300

    test bot api

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const restify = require("restify");
+const builder = require("botbuilder");
+const axios = require('axios');
+
+// Configuração do Server via Restify:
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log("%s Aplicação executando na porta %s", server.name, server.url);
+});
+
+// Criação do chat connector para comunicar com o serviço do Bot Framework:
+const connector = new builder.ChatConnector({
+  appId: "",
+  appPassword: ""
+});
+
+// Endpoint para executar as mensagens para os usuários
+server.post("/api/messages", connector.listen());
+
+const bot = new builder.UniversalBot(connector);
+
+
+
+// Bloco de Dialogs: 
+bot.dialog("/", [session => {
+    builder.Prompts.text(session, "Oi! Para reportar algum incidente digite 1");
+ }, (session, results) => {
+    let resposta = results.response;
+    if (resposta == '1') {
+        session.beginDialog("/reportIssue");
+    }
+    else {
+        session.beginDialog("/");
+    }
+ }]);
+ 
+ bot.dialog("/reportIssue", [session => {
+    builder.Prompts.text(session, "Por favor descreva o que ocorreu");
+ }, (session, results) => {
+    let incidente = results.response;
+    session.endDialog(`Obrigado por avisar, incidente registrado com sucesso.`);
+ }]);
